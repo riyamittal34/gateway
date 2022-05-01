38cd8ecdf34d5ddf9b42634dd56cc63df75f8134
unknown <riyamittal34@gmail.com>
2022-05-01 15:18:49 +0530

    Adding structure and configuration for api gateway

---
+++ src/test/java/com/stock/market/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.market.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
